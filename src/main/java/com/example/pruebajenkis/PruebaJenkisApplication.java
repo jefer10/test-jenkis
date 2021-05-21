4ef34aceabb8e7269ea8b4344551fa6088799757
Jefersson Sebastian Rondon Sanabria - Ceiba Software <jefersson.rondon@ceiba.com.co>
2021-05-21 10:16:50 -0500

    primer commit

---
+++ src/main/java/com/example/pruebajenkis/PruebaJenkisApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebajenkis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaJenkisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaJenkisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pruebajenkis/PruebaJenkisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebajenkis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaJenkisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
